97bde74e291e5da0866e5d0135ec30bb9fd5768b
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-04-02 19:28:28 -0700

    commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const faker = require("faker");
+
+const express = require("express");
+
+const app = express();
+const port = 8000;
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`express server running on port ${port}`));
